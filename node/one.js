d7a1ef9edd978e3727bcc54d15311689c79ee2c7
akmanil <maharanaanil424@gmail.com>
2025-06-16 18:05:15 +0530

    file system of the node

--- node/one.js
+++ node/one.js
@@ -1,14 +1,22 @@
 var fs = require("fs");
 
+//this is used for create a file and update file
 fs.appendFile("newFile.txt" , "Hello world" , (err)=>{
    if(err) throw err;
    console.log("Saved !");
 })
+//this file is used for create a file only
 fs.open("newfile1.txt" , "w" ,(err ,data) =>{
     if(err) throw err;
     console.log("Saved!");
 })
+//this is used for create a file and update file
 fs.writeFile("newFile1.txt" ,"Hello this is my new content writting using fs of writeFile" ,(err) =>{
     if (err) throw err;
     console.log("Saved3!");
+})
+//this is used for delete a file
+fs.unlink("newFile1.txt" ,(err)=> {
+    if(err) throw err;
+    console.log("File Deleted");
 })
